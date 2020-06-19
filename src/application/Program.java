4bf9bf7df07d8c80eeca4ce11eb7e352a45425e7
ThomasPeruch <peruchthomas@gmail.com>
2020-06-19 11:27:41 -0300

    Implementando interface

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import utility.UpperCaseName;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Playstation 4",2300.00));
+		list.add(new Product("Playstation 3",800.00));
+		list.add(new Product("Smarthphone",1500.00));
+		list.add(new Product("Notebook",1800.00));
+		list.add(new Product("Cabo HDMI",50.00));
+		list.add(new Product("Headset",200.00));
+		
+		List<String> namesInCapsLock = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		namesInCapsLock.forEach(System.out::println);
+	}
+}
+
